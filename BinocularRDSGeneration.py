3ca0785e9c53fbdbdc8c37dea6c6d0b2b100004f
YoRHa <89383948+Encore63@users.noreply.github.com>
2022-07-17 16:03:36 +0800

    Add files via upload

---
+++ BinocularRDSGeneration.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def DrawRDS(MaxDiffPixel, axe_left: plt.Axes, axe_right: plt.Axes) -> None:
+    iDotSize = 6
+    iDotCount: int = 1000
+    dots = np.zeros([3, iDotCount])  # type: np.ndarray
+    # width, height = (1920, 1080)
+    # MM_Per_Pixel_W = iMonitorWidth / width
+    # MM_Per_Pixel_H = iMonitorHeight / height
+    a = 375 * 0.3
+    xmax = a
+    ymax = xmax
+    MinDisparityPixel = 0
+
+    # color = list()
+    # zero = list()
+
+    color = np.zeros([3, iDotCount])
+    for i in range(0, iDotCount):
+        if i % 2 == 0:
+            # black
+            color[0, i] = 0
+            color[1, i] = 0
+            color[2, i] = 0
+        else:
+            # white
+            color[0, i] = 255
+            color[1, i] = 255
+            color[2, i] = 255
+
+    zero = np.zeros([1, iDotCount])
+
+    dots[0, :] = 2 * xmax * np.random.random(iDotCount) - xmax
+    dots[1, :] = 2 * ymax * np.random.random(iDotCount) - ymax
+    dots[2, :] = (np.abs(dots[1, :]) / xmax) * (MaxDiffPixel - MinDisparityPixel)
+
+    xy_1 = dots[0:2, :] - np.append(dots, values=zero, axis=0)[2:, :]
+    xy_2 = dots[0:2, :] + np.append(dots, values=zero, axis=0)[2:, :]
+
+    color = color.T / 1000
+    colors = list()
+    for c in color:
+        if c.tolist() == [0.255, 0.255, 0.255]:
+            colors.append('white')
+        elif c.tolist() == [0, 0, 0]:
+            colors.append('black')
+
+    axe_left.scatter(xy_1[1, :], xy_1[0, :], c=colors, s=iDotSize)
+    axe_right.scatter(xy_2[1, :], xy_2[0, :], c=colors, s=iDotSize)
+
+
+if __name__ == '__main__':
+    num = 1
+    pixels = np.arange(5.0, 10.0, 0.5)
+
+    for pixel in pixels:
+        # left figure
+        fig_left = plt.figure()  # type: plt.Figure
+        ax_left = fig_left.add_subplot(111)  # type: plt.Axes
+        ax_left.axis("off")
+        fig_left.patch.set_facecolor("grey")
+
+        # right figure
+        fig_right = plt.figure()  # type: plt.Figure
+        ax_right = fig_right.add_subplot(111)  # type: plt.Axes
+        ax_right.axis("off")
+        fig_right.patch.set_facecolor("grey")
+
+        DrawRDS(pixel, ax_left, ax_right)
+        fig_left.show()
+        fig_right.show()
+        plt.show()
+        # fig_left.savefig("./Figures/BinocularRDSFigures/scatter_dots_figure_left_{}.jpg".format(num))
+        # fig_right.savefig("./Figures/BinocularRDSFigures/scatter_dots_figure_right_{}.jpg".format(num))
+
+        plt.close(fig_left)
+        plt.close(fig_right)
+        num += 1
